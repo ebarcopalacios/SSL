940e0c80cbd8a86ae3ca985adbeb729603e2ce9e
Ezequiel Barco Palacios <113112905+ebarcopalacios@users.noreply.github.com>
2023-05-28 22:48:08 -0300

    Update hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void) {
-    FILE *f;
-
-    f = fopen("output.txt", "w");
-    if (f == NULL) {
-		fprintf(stdout, "No se pudo abrir el archivo de escritura\n\n");
-		exit(1);
-	}
-
-    fprintf( f, "Hello, World!\n");
-
-    fclose(f);
-    
-    system("pause");
+int main(void) 
+{
+    printf("Hello, World!\n");
     return 0;
 }
